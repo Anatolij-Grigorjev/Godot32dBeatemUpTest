722d577ed9147f07e5cb8cfc3ab296a26c547782
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-11-24 13:11:58 +0200

    experimenting with physics layers and jumping

--- game/characters/char_move.gd
+++ game/characters/char_move.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
-export (int) var PEAK_JUMP_HEIGHT = 250
+export (int) var PEAK_JUMP_HEIGHT = 75
 export (int) var JUMP_STRENGTH = -200
 
 var is_jumping = false
@@ -12,37 +12,42 @@ var GRAVITY = 98
 
 var velocity = Vector2()
 
-func get_input():
+func get_move_input():
 	velocity = Vector2()
 	if Input.is_action_pressed('move_right'):
 		velocity.x += 1
 	if Input.is_action_pressed('move_left'):
 		velocity.x -= 1
-	if Input.is_action_pressed('move_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('move_up'):
-		velocity.y -= 1
+	if (!is_jumping):
+		if Input.is_action_pressed('move_down'):
+			velocity.y += 1
+		if Input.is_action_pressed('move_up'):
+			velocity.y -= 1
 	velocity = velocity.normalized() * speed
 	
+func get_jump_input():
+	if (Input.is_action_just_pressed('jump')): 
+		start_jump()
+	
+func start_jump():
+	velocity.y += JUMP_STRENGTH
+	is_jumping = true
+	is_ascending = true
+	G.swap_layer_bit(self, G.LAYERS_HERO_GROUND, G.LAYERS_HERO_AIR)
+	last_jump_y = position.y
+
+func _physics_process(delta):
+	get_move_input()
 	if (is_jumping):
 		#finish jump
 		if (position.y >= last_jump_y):
 			is_jumping = false
-			set_collision_layer_bit(2, true)
+			G.swap_layer_bit(self, G.LAYERS_HERO_AIR, G.LAYERS_HERO_GROUND)
 		else:
 			velocity.y += JUMP_STRENGTH if is_ascending else GRAVITY
 			if (last_jump_y - position.y >= PEAK_JUMP_HEIGHT):
 				is_ascending = false
 	
 	else:
-		if (Input.is_action_just_pressed('jump')): 
-			velocity.y += JUMP_STRENGTH
-			is_jumping = true
-			is_ascending = true
-			set_collision_layer_bit(2, false)
-			last_jump_y = position.y
-	
-
-func _physics_process(delta):
-	get_input()
+		get_jump_input()
 	move_and_slide(velocity)
--- game/globals.gd
+++ game/globals.gd
@@ -1,6 +1,14 @@
 extends Node
 
+# CONSTANTS
 const Z_COEF = sin(deg2rad(45))
+
+## LAYERS
+const LAYERS_HERO_GROUND = 0
+const LAYERS_HERO_AIR = 1
+const LAYERS_STATIC_BG = 2
+
+
 
 func y2z(y):
 	return y * Z_COEF
@@ -8,6 +16,10 @@ func y2z(y):
 func z2y(z):
 	return z / Z_COEF
 
+func swap_layer_bit(node, from_layer, to_layer):
+	var temp_layer_bit = node.get_collision_layer_bit(from_layer)
+	node.set_collision_layer_bit(from_layer, node.get_collision_layer_bit(to_layer))
+	node.set_collision_layer_bit(to_layer, temp_layer_bit)
 
 func _ready():
 	print("Global functions y2z and z2y loaded...")
